1663d2b26f900a6b9c54696ad7f0722819b61111
devruhul <ruhulamin262001@gmail.comm>
2021-09-29 11:36:29 +0600

    adited

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,9 +6,9 @@ const Header = () => {
     <div>
         <div className="container">
             <div className='header-area'>
-                <h1 className='title'>Donate For Climate Changing</h1>
-                <p className="description">Climate change includes both global warming driven by human-induced emissions of <br/> greenhouse gases and the resulting large-scale shifts in weather patterns.</p>
-                <h3> Total Budget : <span className="budget-amount">10 Million</span> </h3>
+                <h1 className='title'>Help Poverty People</h1>
+                <p className="description">There are several definitions of poverty depending on the context of the situation it is placed in, and usually references a state or condition in which a person or community lacks the financial resources and essentials for a certain standard of living.</p>
+                <h3> Total Budget : <span className="budget-amount">5 Million</span> </h3>
             </div>
         </div>
     </div>
